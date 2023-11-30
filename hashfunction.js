01b8875b4273ea1a9e37a614c2a12823de45750d
Le Duc Phong <le.duc.phong@gmail.com>
2023-11-29 22:09:40 -0500

    Update hashfunction.js

--- hashfunction.js
+++ hashfunction.js
@@ -12,31 +12,31 @@ let hash_value = "Crypto plays a heart role in blockchain!";
  * Output: 256-bit string
  */
 async function sha256_hash(str) {
-    return bytesToHex(await sha256(utf8ToBytes(str)));
+    console.log("Using SHA256 => ", bytesToHex(await sha256(utf8ToBytes(str))));
 }
 /** 
  * Input: a string of arbitrary length
  * Output: 256-bit string
  */
 async function keccak_hash(str) {
-    return bytesToHex(await keccak256(utf8ToBytes(str)));
+    console.log("Using Keccak256 => ", bytesToHex(await keccak256(utf8ToBytes(str))));
 }
 /** 
  * Input: a string of arbitrary length
  * Output: 160-bit string
  */
 async function ripemd160_hash(str) {
-    return bytesToHex(await ripemd160(utf8ToBytes(str)));
+    console.log("Using ripemd160 => ", bytesToHex(await ripemd160(utf8ToBytes(str))));
 }
 /** 
  * Input: a string of arbitrary length
  * Output: 512-bit string
  */
 async function blake2b_hash(str) {
-    return bytesToHex(await blake2b(utf8ToBytes(str)));
+    console.log("Using Blake2b => ", bytesToHex(await blake2b(utf8ToBytes(str))));
 }
 
-console.log("Using SHA256 => ", sha256_hash(hash_value));
-console.log("Using Keccak256 => ", keccak_hash(hash_value));
-console.log("Using ripemd160 => ", ripemd160_hash(hash_value));
-console.log("Using Blake2b => ", blake2b_hash(hash_value));
+sha256_hash(hash_value);
+keccak_hash(hash_value);
+ripemd160_hash(hash_value);
+blake2b_hash(hash_value);
